e8ffb2b80f5da7aa5ccf7d86bcef9462e7f01c4c
mcgi0145 <sk8mg@hotmail.com>
2015-12-01 16:27:46 -0500

    Final
    Done

---
+++ FacebookLogin/www/js/app.js
@@ -0,0 +1,81 @@
+angular.module('starter', ['ionic', 'starter.controllers', 'satellizer'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+.config(function($stateProvider, $urlRouterProvider, $authProvider) {
+
+  $authProvider.facebook({
+    clientId: '1519787878334169',
+    name: 'facebook',
+    url: '/auth/facebook',
+    authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
+    redirectUri: window.location.origin + '/',
+    requiredUrlParams: ['display', 'scope'],
+    scope: ['email'],
+    scopeDelimiter: ',',
+    display: 'popup',
+    type: '2.0',
+    popupOptions: { width: 580, height: 400 },
+      responseType: 'token'
+  });
+
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
